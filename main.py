0e8737d802a7927ca55ccaabaa268adf24f2a0d9
klaas8 <132535742+klaas8@users.noreply.github.com>
2026-01-04 01:56:26 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -140,5 +140,8 @@ for duo in ACCOUNTS.split(","):
 IP_LIST.update([ip for ip in IPS.split("\n") if ip.strip()])
 if accounts_list:
     lo()
+    print("可用ip:")
+    for proxy, req_time in successful_proxies:
+        print(f"{proxy} - {req_time}ms")
 if successful_proxies:
     start()
